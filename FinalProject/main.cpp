668c98d700f5a06203dedbe1ffbe6745e2f96ad5
noammansur <noammansur@gmail.com>
2016-08-20 14:14:21 +0300

    Add files via upload

--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-
-using namespace std;
+#include "SPImageProc.h"
+#include <opencv2/imgproc.hpp>//calcHist
+#include <opencv2/core.hpp>//Mat
+#include <opencv2/highgui.hpp>
+#include <vector>
+#include <iostream>
+using namespace cv;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Mat src;
+
+	/// Load image
+	src = imread("./images/img6.png", CV_LOAD_IMAGE_COLOR);
+    imshow("img", src);
+    waitKey(0);
+
     return 0;
 }
