8be4cce4ed9a73661feac1b1a38cd78707efd1b3
noammansur <noammansur@gmail.com>
2016-08-20 16:52:04 +0300

    Add files via upload

--- FinalProject/SPInit.h
+++ FinalProject/SPInit.h
@@ -1,3 +1,18 @@
 /** feature extraction and tree construction */
 
 
+/** struct designed to hold for each image its features */
+typedef struct sp_img_feats SPImgFeats;
+
+/** Extracts/loads features into array of  SPImgFeats according to config*/
+SPImgFeats** spProduceFeats(const SPConfig config);
+
+/** Extracts features of a certain image into SPImgFeat */
+SPImgFeats* spExtractImgFeats(const SPConfig config,int index);
+
+/** save features of a certain image into file */
+void spWriteImgFeatsFile(SPImgFeats* imgFeats);
+
+/** loads features of a certain image from file */
+SPImgFeats* spLoadImgFeats(const SPConfig config,int index);
+
